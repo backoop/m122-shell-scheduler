8aca4b4f0e6deda4d1f02da44c70769cc223ce57
Fynn Westerath <westerathf@gmail.com>
2023-07-05 20:14:44 +0200

    add function

--- scheduler.sh
+++ scheduler.sh
@@ -5,9 +5,25 @@ target_time="$2"
 time_interval="$1"
 message="$2"
 
+# Function to convert the time interval to seconds
+function convert_to_seconds() {
+  # Extract the time values from the interval
+  seconds=$(echo "$time_interval" | grep -o '[0-9]*s' | awk -F's' '{sum += $1} END {print sum}')
+  minutes=$(echo "$time_interval" | grep -o '[0-9]*m' | awk -F'm' '{sum += $1} END {print sum}')
+  hours=$(echo "$time_interval" | grep -o '[0-9]*h' | awk -F'h' '{sum += $1} END {print sum}')
+  days=$(echo "$time_interval" | grep -o '[0-9]*d' | awk -F'd' '{sum += $1} END {print sum}')
+
+  # Calculate the total time in seconds
+  total_seconds=$((seconds + (minutes * 60) + (hours * 3600) + (days * 86400)))
+
+  # Return the total seconds
+  echo "$total_seconds"
+}
+
+
 # Check if the required parameters are provided
 if { [ -z "$target_date" ] || [ -z "$target_time" ]; } && { [ -z "$time_interval" ] || [ -z "$message" ]; }; then
-  echo "Usage: ./notification_script.sh <date> <time> <message>"
+  # echo "Usage: ./notification_script.sh <date> <time> <message>"
   echo "   or: ./notification_script.sh <time_interval> <message>"
   exit 1
 fi
@@ -31,8 +47,8 @@ if [ -n "$time_interval" ] && [ -n "$message" ]; then
   seconds=$(echo "$time_interval" | grep -o '[0-9]*s' | awk -F's' '{sum += $1} END {print sum}')
   minutes=$(echo "$time_interval" | grep -o '[0-9]*m' | awk -F'm' '{sum += $1} END {print sum}')
 
-  # Calculate the total time in seconds
-  total_seconds=$((seconds + (minutes * 60)))
+  # Calculate the total time in seconds with the function
+  total_seconds=$(convert_to_seconds "$time_interval")
 
   # Sleep for the specified time interval
   sleep "$total_seconds"
